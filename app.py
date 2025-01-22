65fb9de2d4981f8b3daee23b7d06733128acd9bd
AASTHASHARMA111 <saasthas111@gmail.com>
2025-01-23 02:46:00 +0530

    combine

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ from flask import Flask, render_template
 # Load the Excel file
 file_path = r'C:\Users\Aastha\Documents\Aastha sharma task 3\rawdata.xlsx'
 raw_data = pd.read_excel(file_path)
+
+# Combine the 'date' and 'time' columns into a single 'timestamp' column
+raw_data['timestamp'] = pd.to_datetime(raw_data['date'].astype(str) + ' ' + raw_data['time'].astype(str))
